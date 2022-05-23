0c93c07641e11fee0710c85fba58c49c601f2ec0
Michael Davis <jrmdd1496@gmail.com>
2022-05-23 14:11:14 -0500

    commit tree 54b4085d7843a7aa77bd26a21f5a7a1c02519eef

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function createEmployeeRecord (array) {
+
+    let employee = {
+        firstName: array[0],
+        familyName:array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+    return employee;
+}
+
+
+function createEmployeeRecords (array) {
+    return array.map(createEmployeeRecord(e))
+}
